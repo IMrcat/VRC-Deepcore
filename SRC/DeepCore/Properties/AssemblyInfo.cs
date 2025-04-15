e2c391ef7964915bd8f1d5af60188c4ec354d43f
Cat <104047478+IMrcat@users.noreply.github.com>
2025-04-15 03:44:10 -0400

    Skids

---
+++ SRC/DeepCore/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.Versioning;
+using System.Security;
+using System.Security.Permissions;
+using DeepCore;
+using MelonLoader;
+
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: MelonInfo(typeof(Entry), "DeepClient", "1.0", "- Biscuit & Awooochy -", null)]
+[assembly: MelonColor]
+[assembly: MelonGame(null, null)]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
